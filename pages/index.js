e9632c83fa071134a7bcb4b342c5c17495268bbd
Renato-de-oliveira647 <renatooliver647@gmail.com>
2025-10-13 21:58:33 -0300

    aula13-10

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
 import {filmes} from "../data/filmes.js"
 
 function home () {
-    console.log(filmes)
-
-    const primeirofilme =filmes[0]
+    
+    
     const estiloImagem={
         width:"300px"
     };
     return <>
         <h1>filmes</h1>
 
-        <ul>
-            <li> 
-                <p>Nome do filme:</p>
+        <ul> 
+            {
+                filmes.map((filme) => {
+                    return(
+                        <li key={`card-filme-${filme.id}` }> 
+                            <p>Nome do filme:</p>
 
-                <p>Título:{primeirofilme.titulo }</p>
-                <p>Ano:{primeirofilme.ano }</p>
-                <p>Gênero:{primeirofilme.grnero}</p>
-                <p>Nota:{primeirofilme.nota }</p>
+                            <p>Título:{filme.titulo }</p>
+                            <p>Ano:{filme.ano }</p>
+                            <p>Gênero:{filme.grnero}</p>
+                            <p>Nota:{filme.nota }</p>
 
-                <img style={estiloImagem} src={primeirofilme.poster}></img>
-            </li>
+                            <img style={estiloImagem} src={filme.poster}></img>
+                        </li>   
+                    );
+
+                })
+            }
+            
         </ul>
     
     </>
